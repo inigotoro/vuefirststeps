07d43a9c14dd0595c03c538b90fbdbce43f44a25
Inigo Toro Hernandez <Inigo.ToroHernandez@ig.com>
2017-04-25 14:29:41 +0100

    Append v-on:click directive

--- js/main.js
+++ js/main.js
@@ -3,11 +3,15 @@ new Vue({
   data: {
     title: 'Hello World! (Vue)',
     link: 'http://www.google.com',
-    finishedLink: '<a href="http://www.google.com">Google FinishedLink</a>'
+    finishedLink: '<a href="http://www.google.com">Google FinishedLink</a>',
+    clickCounter: 0
   },
   methods: {
   	changeTitle: function(ev) {
     	this.title = ev.target.value;
+    },
+    increaseCounter: function() {
+      this.clickCounter++;
     }
   }
 });
